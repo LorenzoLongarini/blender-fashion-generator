a77515890a4d7b6b40f24ff7778c5d15d131dfc2
Longarini <l.longarini@overside.it>
2024-07-16 18:04:01 +0200

    add texture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy import context
 import sys
 sys.path.append('..src/utils')
 from clear import clean_scene
@@ -11,3 +12,18 @@ path = 'D:/DeepFashion/1-1/model_cleaned.obj'
 imported_object = bpy.ops.wm.obj_import(filepath=path)
 obj_object = bpy.context.selected_objects[0]
 print('Imported name: ', obj_object.name)
+
+mat = bpy.data.materials.new(name="New_Mat")
+mat.use_nodes = True
+bsdf = mat.node_tree.nodes["Principled BSDF"]
+texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+texImage.image = bpy.data.images.load("D:/DeepFashion/1-1/1-1_tex.png")
+mat.node_tree.links.new(bsdf.inputs['Base Color'], texImage.outputs['Color'])
+
+ob = context.view_layer.objects.active
+
+# Assign it to object
+if ob.data.materials:
+    ob.data.materials[0] = mat
+else:
+    ob.data.materials.append(mat)
